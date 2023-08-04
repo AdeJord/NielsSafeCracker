5bf99f6c4cbac672dac969ab8c28c7aa411af199
Adrian Jordan <adejord@gmail.com>
2023-08-04 18:06:03 +0100

    commented a bit out so I can get on Laptop

--- src/components/GuessCode1.js
+++ src/components/GuessCode1.js
@@ -23,12 +23,39 @@ const NumPad = (props) => {
   const classes = useStyles();
   const codeLength = props.charInCode;
   const [guessedCodeState, setGuessedCodeState] = useState(
-    Array(codeLength).fill("")
+    Array(codeLength).fill("") // if you put something in here the numbers will not show in the box (Not and empty string)
   );
   const [showSuccessModal, setShowSuccessModal] = useState(false);
   const [showFailModal, setShowFailModal] = useState(false);
 
+
+// WE NEED TO WORK HERE!!!  SET THE NEW BOXES WITH CORRECTLTY GUESSED NUMBERS (TO GREEN AND THE WRONG ONES TO RED)MAYBE??
+
   const restart = () => {
+
+    //*******************************MY BIT************************************* */
+
+    // compare the guessed code to the code
+    const CheckCode = (props) => {
+      console.log("checkCode");
+      const guessedCode = guessedCodeState.join("");
+      const code = props.code;
+      if (guessedCode === code) {
+        return true;
+        console.log("correct");
+      } else {
+        return false;
+        console.log("incorrect");
+      }
+    };
+
+<CheckCode 
+code='123'/>;
+
+    //*******************************MY BIT************************************* */
+
+
+
     setGuessedCodeState(Array(codeLength).fill(""));
   };
 
@@ -45,19 +72,23 @@ const NumPad = (props) => {
     setShowFailModal(false);
     restart();
   };
-  // need to understand the below line
   const delay = (ms) => new Promise((res) => setTimeout(res, ms));
-  //
+  
 
   const handleNumberClick = async (number) => {
     var setCode = props.code;
     var guessedCode = props.codeToNumber;
 
-    if (guessedCodeState.filter((val) => val === "").length > 0) {
+
+
+
+
+
+    if (guessedCodeState.filter((val) => val === "").length > 0) { // Checks all the boxes are not filled? or filled?
       // Handle the number click event
       const updatedCodeState = [...guessedCodeState]; // Create a new array with the current state values
-      const emptyBoxIndex = updatedCodeState.findIndex((val) => val === "");
-      updatedCodeState[emptyBoxIndex] = number; // Update the clicked number in the corresponding empty box
+      const emptyBoxIndex = updatedCodeState.findIndex((val) => val === ""); // finds the first empty value in the array so it can be replaced with nummber (Below)
+      updatedCodeState[emptyBoxIndex] = number; // Update the clicked number in the corresponding empty box from above
       setGuessedCodeState(updatedCodeState); // Update the state with the new array
       await delay(1500);
 
@@ -73,6 +104,11 @@ const NumPad = (props) => {
       }
     }
   };
+
+
+
+
+
 
   const BoxContainer = () => {
     const renderBoxes = () => {
